a61700c581b3360e3400c530d865b35bfc289e6d
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-09-07 14:33:58 +0530

    add server structure with jwt authentication

---
+++ server/app.js
@@ -0,0 +1,57 @@
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+require("dotenv").config();
+
+mongoose.connect(
+	process.env.DBURL,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false,
+		useCreateIndex: true,
+	},
+	(err) => {
+		console.log(err ? err : "Connected...");
+	}
+);
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/api/users", usersRouter);
+
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	if (err.name == "ValidationError") {
+		res.status(400).json({ err: { body: [err.message] } });
+	}
+	if (err.name == "JsonWebTokenError") {
+		res.status(400).json({ err: { body: [err.message] } });
+	}
+	if (err.name == "MongoError") {
+		res.status(400).json({ err: { body: [err.errmsg] } });
+	}
+	if (err.message == "Not Found") {
+		res.status(404).json({ err: { body: [err.message] } });
+	}
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
+
+	// render the error page
+
+	res.status(err.status || 500);
+	res.json({ error: err.message });
+});
+
+module.exports = app;
---
+++ server/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { hash, compare } = require("bcryptjs");
+
+const userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+	},
+	{ timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+	try {
+		if (this.password && this.isModified("password")) {
+			this.password = await hash(this.password, 10);
+		}
+		next();
+	} catch (error) {
+		next(error);
+	}
+});
+
+userSchema.methods.verifyPassword = async function (password, next) {
+	try {
+		return await compare(password, this.password);
+	} catch (error) {
+		next(error);
+	}
+};
+
+module.exports = mongoose.model("User", userSchema);
