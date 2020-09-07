6b96df9239302afc7572612647f1f38e086fa6c0
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-09-07 21:39:58 +0530

    add auth component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./views/common/Home";
+import Signup from "./views/auth/Signup";
+import Signin from "./views/auth/Signin";
 
-function App() {
-	return <div className="App"></div>;
+class App extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<Switch>
+					<Route exact path="/" component={Home} />
+					<Route exact path="/signin" component={Signin} />
+					<Route exact path="/signup" component={Signup} />
+					<Route path="*" component={Signin} />
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
