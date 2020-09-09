ff6009a4c7f47850f8c119fb038df1f519fc6b60
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-09-09 20:29:00 +0530

    add webpack and fix file structure

--- client/src/state/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import rootReducer from "./reducers";
-import thunk from "redux-thunk";
-
-var store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
